78d84dc348391f3ae8665e164cb29606a91fb899
Alisher778 <internet-docs@mail.ru>
2017-01-16 13:08:31 -0500

    added some style effects

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 This is a web site for learning english language for advanced ESL user. User can watch scientific videos and read articles. In addition, should any one has questions, comments and suggestions, he/she can use blog to get help from community.
-click the link here: https://ieltsapp.herokuapp.com/news_pages 
+Click the link here: https://ieltsapp.herokuapp.com/news_pages for Demo!
+  LogIn:    admin@example.com
+  Password: admin123
+  
+When you Login Admin can add, edit and delete all the contents. Users have ability to create a topic and leave comments. 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,17 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+// $(document).ready(function(){
+//     var text = $('#search-input').val();
+//     $('#search-input').keyup(function(event){
+//       event.preventDefault();
+//       if(event.keyCode == 13){
+//         alert(text);
+//       }
+//     })
+
+// })
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require tinymce-jquery
+//= require tinymce
 //= require turbolinks
 //= require_tree .
